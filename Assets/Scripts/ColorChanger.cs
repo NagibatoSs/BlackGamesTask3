cef4813d747fb79a44f7109293f9337c5bc22fed
NagibatoSs <zhuravleva_02@mail.ru>
2022-10-18 01:07:38 +0400

    Outline1, RGB values from selected

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace ChangeColor
 {
     public class ColorChanger : MonoBehaviour
     {
-        // Start is called before the first frame update
+        [SerializeField] private TMP_Text _rValue;
+        [SerializeField] private TMP_Text _gValue;
+        [SerializeField] private TMP_Text _bValue;
+        private ItemOutliner _itemOutliner;
+        private Color _color;
         void Start()
         {
-        
+            _itemOutliner = GetComponent<ItemOutliner>();
+            _itemOutliner.OnSelect.AddListener(SetColors);
         }
 
-        // Update is called once per frame
-        void Update()
+         private void SetColors()
         {
-        
+            Debug.Log("set");
+            _color = _itemOutliner.Color;
+            _rValue.text = Mathf.Round(_color.r*255).ToString();
+            _gValue.text = Mathf.Round(_color.g*255).ToString();
+            _bValue.text = Mathf.Round(_color.b*255).ToString();
         }
     }
 }
--- Assets/Scripts/ItemOutliner.cs
+++ Assets/Scripts/ItemOutliner.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+using UnityEngine.UI;
 
 namespace ChangeColor
 {
     public class ItemOutliner : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+        private bool isSelected=false;
+        public Color Color {get; private set;}
+       // UnityEvent OnSelect сделать отмену селекта других  
+       //и добавить какое то выделение выбранного
+        public UnityEvent OnSelect;
+
+        public void SelectItem() 
         {
-        
+            Color = GetComponent<Image>().color;
+            OnSelect.Invoke();
+            isSelected=true;
+            Debug.Log(this.gameObject.name+" selected");
         }
 
-        // Update is called once per frame
-        void Update()
+        private void OnMouseDown() 
         {
-        
+            Color = gameObject.GetComponent<MeshRenderer>().material.color;
+            OnSelect.Invoke();
+            isSelected=true;
+            Debug.Log(this.gameObject.name+" selected");
         }
     }
 }
