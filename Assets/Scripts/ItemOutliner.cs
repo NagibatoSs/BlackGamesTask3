db677f006dfe43d375129652912fc54ebbefdf34
NagibatoSs <zhuravleva_02@mail.ru>
2022-10-22 19:20:06 +0400

    animation, effects for selected items

--- Assets/Scripts/ItemOutliner.cs
+++ Assets/Scripts/ItemOutliner.cs
@@ -10,25 +10,32 @@ namespace ChangeColor
     public class ItemOutliner : MonoBehaviour
     {
         [HideInInspector] public bool IsSelected=false;
-        public Color Color {get; private set;}
         public ItemOutliner[] items;
        // UnityEvent OnSelect сделать отмену селекта других  
        //и добавить какое то выделение выбранного
         public UnityEvent OnSelect;
+        [SerializeField] private GameObject _selectedVFXPrefab;
+        private GameObject _vfx;
 
         public void SelectItem() 
         {
-            Color = GetComponent<Image>().color;
             OnSelect.Invoke();
             IsSelected=true;
+            _vfx = Instantiate(_selectedVFXPrefab,transform.position,Quaternion.identity);
+            _vfx.transform.parent = transform;
+            _vfx.transform.position+= new Vector3(0,0,-1);
+            _vfx.transform.localScale = new Vector3(5,5,5);
             Debug.Log(this.gameObject.name+" selected");
         }
 
         private void OnMouseDown() 
         {
-            Color = gameObject.GetComponent<MeshRenderer>().material.color;
             OnSelect.Invoke();
             IsSelected=true;
+            _vfx = Instantiate(_selectedVFXPrefab,transform.position,Quaternion.identity);
+            _vfx.transform.parent = transform;
+            _vfx.transform.position+= new Vector3(0,0,-1);
+            _vfx.transform.localScale = new Vector3(5,5,5);
             Debug.Log(this.gameObject.name+" selected");
         }
 
@@ -37,6 +44,7 @@ namespace ChangeColor
             foreach (var item in items)
             {
                 item.IsSelected=false;
+                Destroy(item._vfx);
                 Debug.Log(item.gameObject.name+" unselected");
             }
         }
